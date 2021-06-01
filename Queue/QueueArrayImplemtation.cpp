8cb2b0fd34ef4ee14f767e7b683f74c56f68ce7a
Nikesh Varma <55392033+nikeshvarma@users.noreply.github.com>
2021-06-01 15:41:25 +0530

    Create QueueArrayImplemtation.cpp

---
+++ Queue/QueueArrayImplemtation.cpp
@@ -0,0 +1,96 @@
+// Queue array implementation
+
+#include<iostream>
+using namespace std;
+
+#define MAX 10
+
+// Global Variables
+int queue[MAX], front = 0, rear = 0;
+
+
+void enqueue() {
+    int value;
+
+    if (rear != MAX-1) {
+        cout<<"Enter Value: ";
+        cin>>value;
+        queue[rear++] = value;
+    } else {
+        cout<<"Queue is full"<<endl;
+    }
+}
+
+int dequeue() {
+    int popItem = NULL;
+
+    if (front == rear) {
+        cout<<"Queue is empty"<<endl;
+    } else {
+        popItem = queue[front++];
+        return popItem;
+    }
+    return popItem;
+}
+
+void queueFront() {
+    cout<<queue[front]<<endl;
+}
+
+void queueRear() {
+    cout<<queue[rear - 1]<<endl;
+}
+
+void display() {
+    cout<<"Queue: ";
+    for (int i = front; i< rear; i++)
+        cout<<queue[i]<<" ";
+}
+
+int main()
+{
+    int choice;
+
+    while(1) {
+
+        cout<<endl<<"*************** MENU ***************"<<endl<<endl;
+        cout<<"1. Enqueue"<<endl;
+        cout<<"2. Dequeue"<<endl;
+        cout<<"3. Display"<<endl;
+        cout<<"4. Front"<<endl;
+        cout<<"5. Rear"<<endl;
+        cout<<"6. Exit"<<endl;
+        cout<<endl<<"Enter Your Choice: ";
+        cin>>choice;
+
+        switch (choice) {
+        case 1:
+            enqueue();
+            break;
+
+        case 2:
+            dequeue();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        case 4:
+            queueFront();
+            break;
+
+        case 5:
+            queueRear();
+            break;
+
+        case 6:
+            exit(0);
+            break;
+
+        default:
+            cout<<"Invalid Option"<<endl;
+        }
+    }
+    return 0;
+}
